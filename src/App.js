7e927314b2d0b8a915149ebc545407ca16953f77
unknown <harizsharizad@gmail.com>
2023-05-22 18:57:40 +0800

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import {Button} from 'antd';
 
 function App() {
   return (
-    <div>
-      <h1>Bank Darah MY</h1>
-        <Button type="primary">Primary Button</Button>
+    <div
+    className="flex h-screen justify-center items-center bg-blue-500"
+    >
+      <h1 className="text-white">Bank Darah MY</h1>
     </div>
   );
 }
