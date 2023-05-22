a80608c76fcd10c9de8c9917811fd53e9d5856a3
unknown <harizsharizad@gmail.com>
2023-05-22 19:20:54 +0800

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div
-    className="flex h-screen justify-center items-center bg-blue-500"
-    >
-      <h1 className="text-white">Bank Darah MY</h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            </Routes>
+      </BrowserRouter>
   );
 }
 
