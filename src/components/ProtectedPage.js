93d5ba20f45299d34e4d4306f9e51c7d2f80a29e
HARIZ <harizshariza@gmail.com>
2023-05-25 18:05:26 +0800

    layout

--- src/components/ProtectedPage.js
+++ src/components/ProtectedPage.js
@@ -39,8 +39,37 @@ function ProtectedPage({children}) {
     return (
     currentUser && (
         <div>
-        <h1>Welcome {getLoggedInUserName(currentUser)}</h1>
-        {children}
+            {/*{Header}*/}
+            <div className="flex justify-between items-center bg-primary text-white px-5 py-3">
+                <div>
+                    <h1 className="text-2xl">
+                        BLOOD BANK CYBERJAYA
+                    </h1>
+                    <span className="text-xs">
+                            {currentUser.userType.toUpperCase()}
+                </span>
+
+                </div>
+
+                <div className="flex items-center gap-1">
+                    <i className="ri-shield-user-line"></i>
+                    <div className="flex flex-col">
+                        <span className="mr-5 text-md cursor-pointer">
+                            {getLoggedInUserName(currentUser).toUpperCase()}</span>
+                    </div>
+
+                    <i className="ri-logout-circle-r-line ml-5 cursor-pointer"
+                    onClick={() => {
+                        localStorage.removeItem("token");
+                        navigate("/login");
+                    }}
+                    ></i>
+
+                </div>
+            </div>
+
+            {/*{body}*/}
+            <div className="p-5">{children}</div>
     </div>
         )
     );
